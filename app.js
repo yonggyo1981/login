2f832b7fae4bcee285bf2a0df1fecb867c89245f
yonggyo1981 <yonggyo1981@gmail.com>
2021-06-07 11:21:57 +0900

    .

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const morgan = require('morgan');
 const nunjucks = require('nunjucks');
 const methodOverride = require('method-override');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const logger = require('./lib/logger');
+const { sequelize } = require('./models');
+
+/** 라우터 */
+const indexRouter = require('./routes');
 
 dotenv.config();
 
@@ -17,16 +23,56 @@ nunjucks.configure('views', {
 		watch : true,
 });
 
+/** 데이터베이스 연결 */
+sequelize.sync({ force : false})
+	.then(() => {
+		logger("데이터베이스 연결 성공");
+	})
+	.catch((err) => {
+		logger(err.message, "error");
+		logger(err.stack, "error");
+	});
+
 if (process.env.NODE_ENV == 'production') {
 	app.use(morgan('combined'));
 } else {
 	app.use(morgan('dev'));
 }
+
+app.use(cookieParser(process.env.COOKIE_SECRET)); // CookieParser 미들웨어 등록
+
+/** 세션 기본 설정 */
+app.use(session({
+	resave : false,
+	saveUninitialized : true,
+	secret : process.env.COOKIE_SECRET,
+	cookie : {
+		httpOnly : true,
+		secure : false,
+	},
+	name : 'yhsession',
+}));
+
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
 
+/** 공통 라우터 */
+app.use((req, res, next) => {
+	console.log(req.url);
+	next();
+});
+
+/** 라우터 등록 */
+app.use(indexRouter);
+
+// 없는 페이지 처리 
+app.use((req, res, next) => {
+	const error = new Error(`${req.method} ${req.url}는 없는 페이지 입니다.`);
+	error.status = 404;
+	next(error);
+});
 
 app.use((err, req, res, next) => { // 오류처리 미들웨어 
 	err.status = err.status || 500;
