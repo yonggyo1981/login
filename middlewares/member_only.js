68925bc8d08d0d021c715adbe824c73098398a8e
yonggyo1981 <yonggyo1981@gmail.com>
2021-06-07 18:11:03 +0900

    .

--- middlewares/member_only.js
+++ middlewares/member_only.js
@@ -6,6 +6,7 @@ const { alert } = require('../lib/common');
 */
 module.exports.memberOnly = (req, res, next) => {
 		if (!req.isLogin) {
+			res.status(401);
 			return alert('회원전용 페이지 입니다.', res, -1);
 		}
 			
@@ -18,8 +19,22 @@ module.exports.memberOnly = (req, res, next) => {
 */
 module.exports.guestOnly = (req, res, next) => {
 		if (req.isLogin) {
+			res.status(401);
 			return alert('로그인한 회원은 접속할 수 없습니다.', res, -1);
 		}
 		
 		next();
+};
+
+/**
+* 관리자 전용 페이지 미들웨어 
+*
+*/
+module.exports.adminOnly = (req, res, next) => {
+	if (!req.isLogin || !req.member.isAdmin) {
+		res.status(401); 
+		return alert('페이지 접속 권한이 없습니다.', res, -1);
+	}
+	
+	next();
 };
