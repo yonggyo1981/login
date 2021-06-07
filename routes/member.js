3807cab8f824ca053f4b8deb557f3cbea0215ba5
yonggyo1981 <yonggyo1981@gmail.com>
2021-06-07 12:16:12 +0900

    .

--- routes/member.js
+++ routes/member.js
@@ -1,5 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+/** /member/join  */
+router.route("/join")
+		/** 회원 가입 양식 */
+		.get((req, res, next) => {
+			
+		})
+		/** 회원 가입 처리 */
+		.post((req, res, next) => {
+			
+		});
+
+/** /member/login */
+router.route('/login')
+		/** 로그인 양식 */
+		.get((req, res, next) => {
+			
+		})
+		/** 로그인 처리 */
+		.post((req, res, next) => {
+			
+		});
+
+/** /member/logout */
+router.get('/logout', (req, res, next) => {
+	
+});
 
 module.exports = router;
