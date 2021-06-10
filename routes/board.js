c6b81c8455ab586eaac508ac0864d12c034c3455
yonggyo1981 <yonggyo1981@gmail.com>
2021-06-10 15:58:22 +0900

    .

--- routes/board.js
+++ routes/board.js
@@ -3,6 +3,7 @@
 * /board
 */
 const board = require('../models/board');
+const { boardConfig } = require('../middlewares/board_config');
 const express = require('express');
 const router = express.Router();
 
@@ -10,14 +11,12 @@ const router = express.Router();
 /** 게시글 작성(양식, DB 처리), 수정, 삭제  - /board */
 router.route('/:id')
 		/** 작성 양식 - id (게시판 아이디) */
-		.get(async (req, res, next) => {
-			const id = req.params.id;
-			const config = await board.getBoard(id);
-			const data = { config };
+		.get(boardConfig, async (req, res, next) => {
+			const data = { config : req.boardConfig };
 			return res.render('board/form', data);
 		})
 		/** 작성 처리 - id (게시판 아이디) */
-		.post((req, res, next) => {
+		.post(boardConfig, (req, res, next) => {
 			
 		})
 		/** 수정 - id (게시글 번호) */
@@ -31,9 +30,9 @@ router.route('/:id')
 
 
 /** 게시글 목록 */
-router.get("/list/:id", (req, res, next) => {
-	
-	return res.send("");
+router.get("/list/:id", boardConfig, async (req, res, next) => {
+	const data = { config : req.boardConfig };
+	return res.render('board/list', data);
 });	
 
 
