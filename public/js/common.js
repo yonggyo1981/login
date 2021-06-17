ad7e53dcb9c931e6c879d1c0db81dbe63c58131f
yonggyo1981 <yonggyo1981@gmail.com>
2021-06-17 13:21:49 +0900

    .

--- public/js/common.js
+++ public/js/common.js
@@ -2,4 +2,9 @@ $(function() {
 	$("body").on("change", ".file_upload_form input[type='file']", function() {
 		$(this).closest("form").submit();
 	});
+	
+	/** 상품코드 자동생성 */
+	$(".generate_goodsCd").click(function() {
+		
+	});
 });
